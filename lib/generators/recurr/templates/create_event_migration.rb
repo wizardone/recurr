c22c40d8a2ab92eeb0598e13c065c1186154aa11
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-11-17 08:59:04 +0200

    add additional fields to the migration

--- lib/generators/recurr/templates/create_event_migration.rb
+++ lib/generators/recurr/templates/create_event_migration.rb
@@ -3,6 +3,10 @@ class CreateRecurringEvents < ActiveRecord::Migration<%= migration_version %>
     create_table :recurring_events do |t|
       t.string :name
       t.text :description
+      t.integer :day
+      t.integer :week
+      t.integer :month
+      t.boolean :reminder
 
       t.timestamps
     end
