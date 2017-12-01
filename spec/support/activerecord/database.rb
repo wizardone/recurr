74d52473a1cd18ede468da27cd7c21048da38faf
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-12-01 15:31:11 +0200

    establish proper postgresql connection for testing

--- spec/support/activerecord/database.rb
+++ spec/support/activerecord/database.rb
@@ -3,4 +3,11 @@ require 'yaml'
 db_adapter = ENV['RECURR_DB_ADAPTER'] || 'postgresql'
 config = YAML.load_file(File.expand_path('../database_config.yml', __dir__))[db_adapter]
 
-ActiveRecord::Base.establish_connection(config)
+#begin
+#  ActiveRecord::Base.connection
+#rescue Exception => e
+  # Establish connection with the postgres default postgres owner, then create
+  # the actual database with the desired name
+  ActiveRecord::Base.establish_connection(config.merge('database' => 'postgres'))
+  ActiveRecord::Base.connection.create_database(config['database'])
+#end
