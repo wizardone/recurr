03647707f36606c433aa1c784466d55f5f9a6992
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-01-19 11:32:06 +0200

    calculate next daily event

--- lib/recurr/events/daily.rb
+++ lib/recurr/events/daily.rb
@@ -3,12 +3,20 @@ module Recurr
     class Daily < Base
 
       def current
-        on, at = options[:on], options[:at]
+        at = options[:at]
         start_day = starts.day
         start_day += 1 if starts.hour > at
         current_time = Time.new
 
         Time.new(current_time.year, current_time.month, start_day, at)
+      end
+
+      def next
+        at = options[:at]
+        next_day = current.day + 1
+        current_time = Time.new
+
+        Time.new(current_time.year, current_time.month, next_day, at)
       end
     end
   end
--- spec/daily_event_spec.rb
+++ spec/daily_event_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Recurr::Events::Daily do
   describe '#current' do
     it 'gets the current recurring event for the same day' do
       Timecop.freeze(Time.local(2018, 1, 1, 10)) do
-        event = described_class.new({ on: 5, at: 13 })
+        event = described_class.new({ at: 13 })
         expected_time = Time.new(2018, 1, 1, event.options[:at])
 
         expect(event.current).to eq(expected_time)
@@ -14,11 +14,27 @@ RSpec.describe Recurr::Events::Daily do
 
     it 'gets the current recurring event for the next day' do
       Timecop.freeze(Time.local(2018, 1, 1, 20)) do
-        event = described_class.new({ on: 5, at: 13 })
+        event = described_class.new({ at: 13 })
         expected_time = Time.new(2018, 1, 2, event.options[:at])
 
         expect(event.current).to eq(expected_time)
       end
+    end
+  end
+
+  describe '#next' do
+    it 'gets the next recurring event' do
+      event = described_class.new({ at: 20 })
+      expected_time = Time.new
+
+      expect(event.next).to eq(
+        Time.new(
+          expected_time.year,
+          expected_time.month,
+          expected_time.day + 1,
+          event.options[:at]
+        )
+      )
     end
   end
 end
