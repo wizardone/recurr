3751c1b6337c451ac4eaabc4a3159ee0b88b0fe3
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-01-24 20:40:11 +0200

    date calculations

--- lib/recurr/events/daily.rb
+++ lib/recurr/events/daily.rb
@@ -4,11 +4,17 @@ module Recurr
 
       def current
         at = options[:at]
-        start_time = Time.new(starts.year, starts.month, starts.wday)
+        start_time = Time.new(starts.year, starts.month, starts.day)
         start_day = starts.wday
+        start_month = starts.month
         start_day = calculate_next_week_day(start_day) if starts.hour > at
 
-        Time.new(start_time.year, start_time.month, start_day, at)
+        if month_end?(start_time)
+          start_month += 1
+          start_day = 1
+        end
+
+        Time.new(start_time.year, start_month, start_day, at)
       end
 
       def next
@@ -24,6 +30,15 @@ module Recurr
       def calculate_next_week_day(day)
         return 1 if day == 6
         return day + 1
+      end
+
+      def month_end?(start_time)
+        day_of_month = start_time.mday
+        month_of_year = start_time.month
+
+        return true if day_of_month == 31 && %w(1 3 5 7 8 10 12).map(&:to_i).include?(month_of_year)
+        return true if day_of_month == 30 && %w(4 6 9 11).include?(month_of_year)
+        return false
       end
     end
   end
