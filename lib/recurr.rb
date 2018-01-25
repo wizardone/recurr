d2da3480f6910713aa3d4f0fc0101565077f3ba3
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-01-25 14:31:11 +0200

    move calculations into separate file

--- lib/recurr.rb
+++ lib/recurr.rb
@@ -38,6 +38,7 @@ end
 
 require 'recurr/adapters/active_record'
 require 'recurr/recurring_event'
+require 'recurr/events/calculations'
 require 'recurr/events/base'
 require 'recurr/events/daily'
 require 'recurr/events/weekly'
--- lib/recurr/events/daily.rb
+++ lib/recurr/events/daily.rb
@@ -1,38 +1,5 @@
 module Recurr
   module Events
-
-    YearCalculation = Struct.new(:time) do
-      def end?
-        time.yday == 365 || time.yday == 366
-      end
-    end
-
-    MonthCalculation = Struct.new(:time) do
-      def end?
-        day_of_month = time.mday
-        month_of_year = time.month
-
-        return true if day_of_month == 31 && big_months.include?(month_of_year)
-        return true if day_of_month == 30 && small_months.include?(month_of_year)
-        return true if (day_of_month == 28 || day_of_month == 29) && month_of_year == february
-        false
-      end
-
-      private
-
-      def big_months
-        [1, 3, 5, 7, 8, 10, 12]
-      end
-
-      def small_months
-        [4, 6, 9, 11]
-      end
-
-      def february
-        2
-      end
-    end
-
     class Daily < Base
 
       def current
