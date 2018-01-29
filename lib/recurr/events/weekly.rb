7b912da7dffa0f3276b1196e6306566186e587b0
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-01-29 14:19:38 +0200

    correct week day bug

--- lib/recurr/events/weekly.rb
+++ lib/recurr/events/weekly.rb
@@ -3,10 +3,10 @@ module Recurr
     class Weekly < Base
       def current
         at, on = options[:at], options[:on]
-        start_time = Time.new(starts.year, starts.month, starts.wday)
+        start_time = Time.new(starts.year, starts.month, starts.day)
         start_year = start_time.year
         start_month = start_time.month
-        start_day = start_time.wday
+        start_day = start_time.day
 
         if start_day > on
           diff = start_day - on
--- spec/weekly_event_spec.rb
+++ spec/weekly_event_spec.rb
@@ -12,10 +12,28 @@ RSpec.describe Recurr::Events::Weekly do
       end
     end
 
+    it 'gets the current recurring event for the same week' do
+      Timecop.freeze(Time.local(2018, 1, 10)) do
+        event = described_class.new({ on: 5, at: 13 })
+        expected = Time.new(2018, 1, 12, 13)
+
+        expect(event.current).to eq(expected)
+      end
+    end
+
     it 'gets the current recurring event for the next week' do
       Timecop.freeze(Time.local(2018, 1, 5)) do
         event = described_class.new({ on: 3, at: 13 })
         expected = Time.new(2018, 1, 10, 13)
+
+        expect(event.current).to eq(expected)
+      end
+    end
+
+    it 'gets the current recurring event if next week is in next month' do
+      Timecop.freeze(Time.local(2018, 1, 30)) do
+        event = described_class.new({ on: 1, at: 13 })
+        expected = Time.new(2018, 2, 5, 13)
 
         expect(event.current).to eq(expected)
       end
